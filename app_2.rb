9b6a341b196366aae2c0636f9f3cf5c27a3fb756
Rostom Halaïmia <rostom.halaimia@gmail.com>
2020-04-21 17:54:47 +0200

    indentation

--- app_2.rb
+++ app_2.rb
@@ -24,36 +24,36 @@ while (player1.life_points > 0 || player2.life_points > 0) && user.life_points >
   puts "   b - chercher un healthpack"
   puts ""
   puts "Attaquer un ennemi en vue :"
-  if player1.life_points > 0
-    puts "   0 - #{player1.show_state}"
-  else
-    puts "   #{player1.name} est mort"
-  end
-  if player2.life_points > 0
-    puts "   1 - #{player2.show_state}"
-  else
-    puts "   #{player2.name} est mort"
-  end
+    if player1.life_points > 0
+      puts "   0 - #{player1.show_state}"
+    else
+      puts "   #{player1.name} est mort"
+    end
+    if player2.life_points > 0
+      puts "   1 - #{player2.show_state}"
+    else
+      puts "   #{player2.name} est mort"
+    end
 
   puts ""
 
   user_choice = gets.chomp
   if user_choice == "a"
-    puts ""
-    user.search_weapon
-  elsif user_choice == "b"
-    puts ""
-    user.search_health_pack
-  elsif user_choice == "0"
-    puts ""
-    user.attacks(player1)
-  elsif user_choice == "1"
-    puts ""
-    user.attacks(player2)
-  else
-    puts ""
-    puts "Je n'ai pas compris ton choix, choisis entre a, b, 0 ou 1"
-    user_choice = gets.chomp
+      puts ""
+      user.search_weapon
+    elsif user_choice == "b"
+      puts ""
+      user.search_health_pack
+    elsif user_choice == "0"
+      puts ""
+      user.attacks(player1)
+    elsif user_choice == "1"
+      puts ""
+      user.attacks(player2)
+    else
+      puts ""
+      puts "Je n'ai pas compris ton choix, choisis entre a, b, 0 ou 1"
+      user_choice = gets.chomp
   end
 
   puts ""
@@ -64,7 +64,6 @@ while (player1.life_points > 0 || player2.life_points > 0) && user.life_points >
       puts ""
     end
   end
-
 end
 
 puts ""
@@ -73,8 +72,8 @@ puts "La partie est finie."
 
 if user.life_points > 0
   puts "Bravo, tu les as écrasés !"
-else
-  puts "You loooooooose !"
+  else
+    puts "You loooooooose !"
 end
 
 
